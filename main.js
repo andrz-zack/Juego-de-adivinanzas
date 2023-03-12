4a91e801ee67e7c028319b7b010edbaa44e47ffc
Andrz Zack <guillermo07_chavez07@outlook.es>
2023-03-12 14:01:06 -0500

    Agregando mas scripts para el juego

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ const moviesObject = {
     '📱🍎': 'Jobs',
     '🔍🐟': 'Buscando a Nemo',
 };
+
+const container = document.querySelector('.container');
+const controls = document.querySelector('.controls-container');
+const startButton = document.getElementById('start');
+const letterContainer = document.getElementById('letter-container');
+const userInputSection = document.getElementById('userInputSection');
+const resultText = document.getElementById('result');
+const hints = Object.keys(moviesObject);
